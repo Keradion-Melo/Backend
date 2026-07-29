c061f8025af7d878428384171824119e54b453be
ellay21 <mesudmelaku1@gmail.com>
2026-07-29 10:45:00 +0300

    feat(common): implement standard response transformation interceptor

---
+++ src/common/interceptors/response.interceptor.ts
@@ -0,0 +1,30 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+export interface ApiResponse<T> {
+  statusCode: number;
+  message: string;
+  data: T;
+}
+
+@Injectable()
+export class ResponseInterceptor<T> implements NestInterceptor<T, ApiResponse<T>> {
+  intercept(context: ExecutionContext, next: CallHandler<T>): Observable<ApiResponse<T>> {
+    const httpContext = context.switchToHttp();
+    const response = httpContext.getResponse<{ statusCode: number }>();
+
+    return next.handle().pipe(
+      map((data) => ({
+        statusCode: response.statusCode,
+        message: 'Success',
+        data,
+      })),
+    );
+  }
+}
